48d8a1e5082774e2eaf4eb6435b3b890771f8784
Togald Nilsson <togald.nilsson@gmail.com>
2021-12-12 22:53:02 +0100

    Finished energyoptinator, added test case

--- energyoptinator.py
+++ energyoptinator.py
@@ -2,32 +2,64 @@
 
 import matplotlib.pyplot as plt
 
-class Simulation:    
-    def __init__(self, name = "", grids = {}, sources = {}, sinks = {}, storages = {}, convs = {}):
+class Simulation:
+    def __init__( self, name = ""
+                , grids = {}
+                , entities = {}
+                , storages = {} ):
+        """ Initializes a simulation. The simulation object holds a
+        reference to all grids and entities in the simulation. Every
+        object has a name, and objects are expected to be stored in
+        dictionaries.
+
+        Entities are expected to add themselves to the reference lists.
+        """
         self.name     = name
         self.grids    = grids
-        self.sources  = sources
-        self.sinks    = sinks
+        self.entities = entities
         self.storages = storages
-        self.convs    = convs
     def step(self):
-        for name, grid in self.grids.items(): 
-            grid.powers[name].append(0)
-        for name, source in self.sources.items():
-            source.step()
-        for name, sink in self.sinks.items():
-            sink.step()
-        for name, storage in self.storages.items():
+        for _, entity in self.entities.items():
+            entity.step()
+        for _, grid in self.grids.items():
+            grid.step()
+        for _, storage in self.storages.items():
             storage.step()
-        for name, conv in self.convs.items():
-            conv.step()
+    def plot_grids(self):
+        fig = plt.figure(1)
+        for name, grid in self.grids.items():
+            ax = plt.plot(grid.powers[name], label=name)
+        fig.legend()
+        plt.title(self.name)
+        plt.show()
+    def plot_storages(self):
+        fig = plt.figure(1)
+        for name, storage in self.storages.items():
+            ax = plt.plot(storage.charges, label=name)
+        fig.legend()
+        plt.title(self.name)
+        plt.show()
 
 class Grid:
     def __init__(self, name, sim):
+        """ Creates a new Grid object. The new Grid object holds its
+        name, places a reference to itself in the simulation it belongs
+        to, and contains a dictionary with lists of all contributing
+        powers.
+        """
         self.name = name
+        self.sim = sim
         sim.grids[name] = self
         self.powers = { self.name : [] }
+    def step(self):
+        self.powers[self.name].append(0)
+        for name, power in self.powers.items():
+            if name in self.sim.entities.keys():
+                self.powers[self.name][-1] += power[-1]
     def plot(self):
+        """ Grid.plot() plots a time diagram of all additions and
+        subtractions from the grid, one series per connected entity.
+        """
         fig = plt.figure(1)
         for name, power in self.powers.items():
             ax = plt.plot(power, label=name)
@@ -37,42 +69,91 @@ class Grid:
 
 class SimpleSource:
     def __init__(self, name, sim, grid, power):
+        """ SimpleSource is the simplest possible source, it adds its
+        self.power to its self.grid every time step. There is no unit
+        conversion, no variation.
+        """
         self.name = name
-        sim.sources[name] = self
-        self.grid   = sim.grids[grid]
+        sim.entities[name] = self
+        self.grid = sim.grids[grid]
         self.power = power
         self.powers = { self.grid.name : [] }
         self.grid.powers[self.name] = self.powers[self.grid.name]
     def step(self):
         self.powers[self.grid.name].append(self.power)
-        self.grid.powers[self.grid.name][-1] += self.powers[self.grid.name][-1]
 
 class SimpleSink:
     def __init__(self, name, sim, grid, power):
+        """ SimpleSink is the simplest possible sink, it subtracts its
+        self.power to its self.grid every time step. There is no unit
+        conversion, no variation.
+        """
         self.name = name
-        sim.sinks[name] = self
+        sim.entities[name] = self
         self.grid   = sim.grids[grid]
         self.power = power
         self.powers = { self.grid.name : [] }
         self.grid.powers[self.name] = self.powers[self.grid.name]
     def step(self):
         self.powers[self.grid.name].append(-self.power)
-        self.grid.powers[self.grid.name][-1] += self.powers[self.grid.name][-1]
+
+class SimpleStorage:
+    def __init__(self, name, sim, grid, capacity, initialCharge):
+        """SimpleStorage is the simplest possible storage. There is no
+        over-time losses, no charging or discharging losses, and no
+        unit conversion.
+        """
+        self.name = name
+        sim.storages[name] = self
+        self.grid = sim.grids[grid]
+        self.capacity = capacity
+        self.charges = []
+        self.initialCharge = initialCharge
+        self.powers = { self.grid.name : [] }
+        self.grid.powers[self.name] = self.powers[self.grid.name]
+    def step(self):
+        """ If overflowing:
+        - Set power to be absorbed (self.grid.powers[self.name])
+        - Subtract that from grid (self.powers[self.grid.name])
+        - Set charge to full
+        If completely drained:
+        - Set power to be delivered is equal to current charge
+        -
+        """
+        if not self.charges:
+            self.charges.append(self.initialCharge)
+        else:
+            self.charges.append(self.charges[-1])
+        if self.charges[-1] + self.grid.powers[self.grid.name][-1] > self.capacity:
+            # Storage is overflowing
+            self.powers[self.grid.name].append(self.capacity - self.charges[-1])
+            self.grid.powers[self.grid.name][-1] -= self.powers[self.grid.name][-1]
+            self.charges[-1] = self.capacity
+        elif self.charges[-1] + self.grid.powers[self.grid.name][-1] < 0:
+            # Storage is emptied
+            self.powers[self.grid.name].append(self.charges[-1])
+            self.grid.powers[self.grid.name][-1] += self.powers[self.grid.name][-1]
+            self.charges[-1] = 0
+        else:
+            # Storage is used normally
+            self.powers[self.grid.name].append(-self.grid.powers[self.grid.name][-1])
+            self.grid.powers[self.grid.name][-1] = 0
+            self.charges[-1] -= self.powers[self.grid.name][-1]
 
 class SimpleBoiler:
     def __init__(self, name, sim, fuelGrid, heatGrid, efficiency = 0.85):
         self.name = name
-        sim.convs[name] = self
+        sim.entities[name] = self
         self.fuelGrid = sim.grids[fuelGrid]
         self.heatGrid = sim.grids[heatGrid]
         self.efficiency = efficiency
-        self.powers = { self.fuelGrid.name : [] 
+        self.powers = { self.fuelGrid.name : []
                       , self.heatGrid.name : []
                       }
         self.fuelGrid.powers[self.name] = self.powers[self.fuelGrid.name]
         self.heatGrid.powers[self.name] = self.powers[self.heatGrid.name]
     def step(self):
-        for name, power in self.powers.items(): 
+        for name, power in self.powers.items():
             power.append(0)
         if self.heatGrid.powers[self.heatGrid.name][-1] < 0:
             self.powers[self.heatGrid.name][-1] -= self.heatGrid.powers[self.heatGrid.name][-1]
@@ -80,13 +161,18 @@ class SimpleBoiler:
             self.powers[self.fuelGrid.name][-1] += self.powers[self.heatGrid.name][-1] / self.efficiency
             self.fuelGrid.powers[self.fuelGrid.name][-1] += self.powers[self.fuelGrid.name][-1]
 
-s = Simulation('test')
-Grid('Heat', s)
-Grid('Wood', s)
-SimpleSource('Pwr1', s, 'Heat', 20)
-SimpleSink('Drain', s, 'Heat', 50)
-SimpleBoiler('Boiler1', s, 'Wood', 'Heat')
-for _ in range(8760):
-    s.step()
-for name, grid in s.grids.items():
-    grid.plot()
+def dem1():
+    s = Simulation('test')
+    Grid('Heat', s)
+    SimpleSource('Heater1', s, 'Heat', 20)
+    SimpleSink('Drain1', s, 'Heat', 50)
+    SimpleStorage('Acc1', s, 'Heat', 100000, 100000)
+    for _ in range(8760):
+        s.step()
+    for name, grid in s.grids.items():
+        grid.plot()
+    s.plot_grids()
+    s.plot_storages()
+
+if __name__ == '__main__':
+    dem1()
--- test.py
+++ test.py
@@ -1,204 +1,49 @@
 import matplotlib.pyplot as plt
-import energyoptinator as energy
+import energyoptinator as eo
 
-class Simulation:    
-    def __init__(self, name = "", grids = {}, sources = {}, sinks = {}, convs = {}, storages = {}):
-        self.name     = name
-        self.grids    = grids
-        self.sources  = sources
-        self.sinks    = sinks
-        self.storages = storages
-        self.convs    = convs
-    def step(self):
-        for name, grid in self.grids.items(): 
-            grid.powers.append(0)
-        for name, source in self.sources.items():
-            source.step()
-        for name, sink in self.sinks.items():
-            sink.step()
-        for name, storage in self.storages.items():
-            storage.step()
-        for name, conv in self.convs.items():
-            conv.step()
-
-class Grid:
-    def __init__(self, name, sim):
-        self.name       = name
-        sim.grids[name] = self
-        self.powers     = []
-        self.sources    = []
-        self.sinks      = []
-        self.storages   = []
-    def plot(self):
-        fig = plt.figure(1)
-        ax = plt.plot(grid.powers, label=gridname)
-        for source in self.sources:
-            ax = plt.plot(source.powers, label=source.name)
-        for sink in self.sinks:
-            ax = plt.plot(sink.powers, label=sink.name)
-        for storage in self.storages:
-            ax = plt.plot(storage.charges, label=storage.name)
-        fig.legend()
-        plt.title(self.name)
-        plt.show()
-
-class Source:
-    def __init__(self, name, sim):
-        self.name         = name
-        sim.sources[name] = self
-
-class SimpleSolar(Source):
+class SimpleSolar:
     def __init__(self, name, sim, grid, irradiance, efficiency, area):
-        Source.__init__(self, name, sim)
-        self.grid       = sim.grids[grid]
-        self.grid.sources.append(self)
+        """ Creates a SimpleSolar power source, taking irradiance data
+        given in [W/m^2], efficiency in absolute numbers, and area in
+        [m^2], and stuffs the power with [W].
+        """
+        self.name = name
+        sim.entities[name] = self
+        self.grid = sim.grids[grid]
         self.irradiance = irradiance
         self.efficiency = efficiency
-        self.area       = area
-        self.powers     = []
-    def step(self):
-        self.powers.append(self.irradiance[len(self.powers)] * self.efficiency * self.area)
-        self.grid.powers[-1] += self.powers[-1]
-
-class Sink:
-    def __init__(self, name, sim, grid):
-        self.name       = name
-        sim.sinks[name] = self
-        self.grid       = sim.grids[grid]
-        self.grid.sinks.append(self)
-
-class SimpleSink(Sink):
-    def __init__(self, name, sim, grid, drain):
-        Sink.__init__(self, name, sim, grid)
-        self.drain  = drain
-        self.powers = []
-    def step(self):
-        self.powers.append(self.drain)
-        self.grid.powers[-1] -= self.powers[-1]
-
-class Conv:
-    def __init__(self, name, sim):
-        self.name       = name
-        sim.convs[name] = self
-
-class Boiler(Conv):
-    def __init__(self, name, sim, inputgrid, outputgrid, efficiency=0.85):
-        Conv.__init__(self, name, sim)
-        self.inputgrid = sim.grids[inputgrid]
-        self.outputgrid = sim.grids[outputgrid]
-        self.inputgrid.sinks.append(self)
-        self.outputgrid.sources.append(self)
-        self.efficiency = efficiency
-        self.powers = []
-    def step(self):
-        self.powers.append(0)
-        if self.outputgrid.powers[-1] < 0:
-            self.powers[-1] += self.outputgrid.powers[-1] / self.efficiency
-            self.inputgrid.powers[-1] += self.powers[-1]
-            self.outputgrid.powers[-1] = 0
-        else:
-            pass
-
-class Storage:
-    def __init__(self, name, sim, grid):
-        self.name          = name
-        sim.storages[name] = self
-        self.grid          = sim.grids[grid]
-        self.grid.storages.append(self)
-
-class IdealBattery:
-    def __init__(self, name, sim, grid, capacity, initialCharge):
-        Storage.__init__(self, name, sim, grid)
-        self.capacity = capacity
-        self.charges  = [initialCharge]
-    def soc(self):
-        return self.charges[-1]/self.capacity
-    def step(self):
-        self.charges.append(self.charges[-1])
-        if self.charges[-1] + self.grid.powers[-1] > self.capacity:
-            self.grid.powers[-1] -= self.capacity - self.charges[-1]
-            self.charges[-1] = self.capacity
-        elif self.charges[-1] + self.grid.powers[-1] < 0:
-            self.grid.powers[-1] += self.charges[-1]
-            self.charges[-1] = 0
-        else:
-            self.charges[-1] += self.grid.powers[-1]
-            self.grid.powers[-1] = 0
-
-def test1():
-    irradiance = [ 13, 24, 41, 90, 114, 108, 102, 105, 88, 90, 53, 16, 13 ]
-    hushel = 500
-    
-    s = Simulation()
-    Grid('El', s)
-    Grid('Ved', s)
-    Grid('Värme', s)
-    SimpleSolar('Solceller', s, 'El', irradiance, 0.2, 30)
-    SimpleSink('Hushållsel', s, 'El', hushel)
-    IdealBattery('Batteri', s, 'El', 50, 25)
-    Boiler('Värmepanna', s, 'Ved', 'El', 0.25)
-    for _ in irradiance:
-        s.step()
-    
-    # Plot
-    for gridname, grid in s.grids.items():
-        grid.plot()
-
-#test1()
-
-class TestSim:
-    def __init__(self):
-        self.grids = {}
-        self.entities = {}
-
-class TestGrid:
-    def __init__(self, name, sim):
-        self.name = name
-        sim.grids[name] = self
-        self.powers = { self.name : [] }
-        self.entities = []
-
-class TestEntity:
-    def __init__(self, name, sim, grid):
-        self.name = name
-        sim.entities[name] = self
-        self.grid   = sim.grids[grid]
-        self.grid.entities.append(self)
-        self.powers = { self.grid.name : [] }
+        self.area = area
+        self.powers = { self.grid.name: [] }
         self.grid.powers[self.name] = self.powers[self.grid.name]
+    def step(self):
+        self.powers[self.grid.name].append(self.irradiance[len(self.powers[self.grid.name])] * self.efficiency * self.area)
 
-class TestEntity2:
-    def __init__(self, name, sim, grid1, grid2):
-        self.name = name
-        sim.entities[name] = self
-        self.grid1   = sim.grids[grid1]
-        self.grid1.entities.append(self)
-        self.grid2   = sim.grids[grid2]
-        self.grid2.entities.append(self)
-        self.powers = { self.grid1.name : []
-                      , self.grid2.name : []
-                      }
-        self.grid1.powers[self.name] = self.powers[self.grid1.name]
-        self.grid2.powers[self.name] = self.powers[self.grid2.name]
+def monthly_to_daily_flat(months_iter):
+    months = [31, 28, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31]
+    days =  []
+    for month, data in zip(months, months_iter):
+        for _ in range(month):
+            days.append(data/month)
+    return days
 
-s = TestSim()
-TestGrid('El', s)
-TestGrid('Värme', s)
-TestEntity('Sol', s, 'El')
-TestEntity2('Panna', s, 'El', 'Värme')
+def test1(initBatt):
+    irradiance = [ 13, 24, 41, 90, 114, 108, 102, 105, 88, 90, 53, 16, 13 ] # kWh/m^2, month
+    irr = monthly_to_daily_flat(irradiance)
+    hushel = 3000/365 # kWh/d
 
-print(s.grids['El'].powers)
-print(s.grids['Värme'].powers)
-print(s.entities['Panna'].powers)
+    s = eo.Simulation()
+    eo.Grid('El', s)
+    SimpleSolar('Solceller', s, 'El', irr, 0.2, 30)
+    eo.SimpleSink('Hushållsel', s, 'El', hushel)
+    eo.SimpleStorage('Batteri', s, 'El', 500, initBatt)
+    for _ in irr:
+        s.step()
 
-s.entities['Panna'].powers['El'].append(2)
+    # Plot
+    for _, grid in s.grids.items():
+        grid.plot()
+    s.plot_storages()
+    return s.storages['Batteri'].charges[-1]
 
-print(s.grids['El'].powers)
-print(s.grids['Värme'].powers)
-print(s.entities['Panna'].powers)
-
-s.grids['Värme'].powers['Panna'].append(2)
-
-print(s.grids['El'].powers)
-print(s.grids['Värme'].powers)
-print(s.entities['Panna'].powers)
+if __name__ == '__main__':
+    test1(test1(500))
